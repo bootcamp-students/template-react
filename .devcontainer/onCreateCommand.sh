651ada2a710a111d485e62d4a915cded779bc372
Jonathan Morford <git@jonathanmorford.com>
2022-10-11 19:23:22 +0000

    Resolve a few console errors/warnings

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+# Change Create React App development web server websocket port to 443 to allow access through the Gitpod proxy
+echo "export PORT=443" >> ~/.bashrc.d/create-react-app_webpack-devserver_gitpod-ports.sh
+echo "export WDS_SOCKET_PORT=443" >> ~/.bashrc.d/create-react-app_webpack-devserver_gitpod-ports.sh
+# Allow ports below 1024 for node without root (we want to use 443)
+sudo setcap 'cap_net_bind_service=+ep' $(which node)
